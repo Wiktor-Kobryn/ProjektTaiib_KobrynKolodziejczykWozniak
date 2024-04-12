28ac2894bbe93d7973f5653953458bb0fe61afad
KarolKolodziejczyk <kk303201@student.polsl.pl>
2024-04-12 17:59:27 +0200

    Poprawki

--- BDDAL/DbDalContext.cs
+++ BDDAL/DbDalContext.cs
@@ -18,7 +18,7 @@ namespace BDDAL
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
             //  optionsBuilder.UseSqlServer("Server=localhost; Database=SklepInternetowyKK; TrustServerCertificate=True;");
 
