ca5fc7df6febb6a32a6759509d288a804728008e
KarolKolodziejczyk <kk303201@student.polsl.pl>
2024-06-26 00:07:11 +0200

    admin
    admin panel

--- Angular/AngularProjekt/src/app/admin-panel/admin-panel.component.ts
+++ Angular/AngularProjekt/src/app/admin-panel/admin-panel.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { UserResponseDTO } from '../model/user.interface';
+import { EventResponseDTO } from '../model/event.interface';
+import { EventsService } from '../events.service';
 
 @Component({
   selector: 'app-admin-panel',
@@ -10,13 +12,55 @@ import { UserResponseDTO } from '../model/user.interface';
 export class AdminPanelComponent implements OnInit {
 
   users: UserResponseDTO[] = [];
-
-  constructor(private userService: UserService) { }
+  usersCreatedInLastWeek: UserResponseDTO[] = [];
+  liczbauserow: number = 0;
+  listyTasks: EventResponseDTO[] = [];
+  eventsCreatedLastWeek: EventResponseDTO[] = [];
+  liczbaTask: number =0;
+  constructor(private userService: UserService, private eventService: EventsService) { }
 
   ngOnInit(): void {
     this.userService.getUsers(1).subscribe({
-      next: (users) => this.users = users,
-      error: (err) => console.error('Error fetching users:', err)
+      next: (users) => {
+        this.users = users;
+        this.liczbauserow = users.length;
+        this.usersCreatedInLastWeek = this.filterUsersByLastWeek(users);
+
+        users.forEach(user => {
+          this.eventService.getUserEvents(user.id).subscribe({
+            next: (events) => {
+              events.forEach(event => {
+                this.listyTasks.push(event);
+                if (this.isEventCreatedLastWeek(event)) {
+                  this.eventsCreatedLastWeek.push(event);
+                  this.liczbaTask = this.eventsCreatedLastWeek.length;
+
+                }
+              });
+            },
+            error: (err) => console.error(`Błąd pobierania wydarzeń użytkownika o ID ${user.id}:`, err)
+          });
+        });
+      },
+      error: (err) => console.error('Błąd pobierania użytkowników:', err)
     });
+  }
+
+  private filterUsersByLastWeek(users: UserResponseDTO[]): UserResponseDTO[] {
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+    return users.filter(user => {
+      const creationDate = new Date(user.creationDate);
+      return creationDate >= oneWeekAgo;
+    });
+  }
+
+  private isEventCreatedLastWeek(event: EventResponseDTO): boolean {
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+    const eventDate = new Date(event.creationDate);
+    
+    return eventDate >= oneWeekAgo;
   }
 }
